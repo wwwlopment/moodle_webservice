52c5127d9fcb5e39f42069cfbd063ffe3e469c53
Bogdan <wwwlopment@gmail.com>
2021-04-27 10:55:24 +0300

    moodle webservice

--- webservice/externallib.php
+++ webservice/externallib.php
@@ -70,7 +70,15 @@ class local_webservice_external extends external_api {
      * @return string welcome message
      */
     public static function get_courses() {
-        return get_courses();
+        $categories = \core_course_category::get_all();
+        $courses = get_courses();
+        $result = array();
+        foreach ($courses as $key => $course) {
+            $result[$key] = $course;
+            $result[$key]->categoryName = key_exists($course->category, $categories) ? $categories[$course->category]->name : '';
+        }
+
+        return $result;
     }
 
     /**
@@ -85,6 +93,7 @@ class local_webservice_external extends external_api {
                     'fullname' => new external_value(PARAM_TEXT, 'multilang compatible fullname'),
                     'format' => new external_value(PARAM_TEXT, 'multilang compatible format'),
                     'category' => new external_value(PARAM_INT, 'category'),
+                    'categoryName' => new external_value(PARAM_TEXT, 'categoryName'),
                     'startdate' => new external_value(PARAM_INT, 'startdate'),
                     'enddate' => new external_value(PARAM_INT, 'enddate'),
                 )
@@ -109,12 +118,13 @@ class local_webservice_external extends external_api {
     public static function get_enrolled_users() {
         $users = get_users();
         $enrolled_users = array();
+
         foreach ($users as $user) {
             $user_courses = enrol_get_users_courses($user->id);
             if (count($user_courses) === 0) {
                 continue;
             }
-            $enrolled_users[$user->id]['userid'] = $user->id;
+            $enrolled_users[$user->id] = (array) $user;
             $enrolled_users[$user->id]['courses'] = $user_courses;
             foreach ($user_courses as $course) {
                 $enrolled_users[$user->id]['courses'][$course->id]->grade = grade_get_course_grade($user->id,
@@ -132,7 +142,13 @@ class local_webservice_external extends external_api {
     public static function get_enrolled_users_returns() {
         return new external_multiple_structure(
             new external_single_structure(array(
-                    'userid' => new external_value(PARAM_RAW, 'id of user'),
+                    'id' => new external_value(PARAM_RAW, 'some user id'),
+                    'username' => new external_value(PARAM_TEXT, 'multilang compatible username'),
+                    'firstname' => new external_value(PARAM_TEXT, 'multilang compatible firstname'),
+                    'lastname' => new external_value(PARAM_TEXT, 'multilang compatible lastname'),
+                    'city' => new external_value(PARAM_TEXT, 'multilang compatible city'),
+                    'description' => new external_value(PARAM_RAW, 'just some text'),
+                    'email' => new external_value(PARAM_EMAIL, 'email'),
                     'courses' => new external_multiple_structure(
                         new external_single_structure(
                             array(
@@ -142,22 +158,6 @@ class local_webservice_external extends external_api {
                                 'visible' => new external_value(PARAM_RAW, '1 means visible, 0 means hidden course'),
                                 'grade' => new external_single_structure(
                                     array(
-                                        'item' => new external_single_structure(
-                                            array(
-                                                'scaleid' => new external_value(PARAM_INT,
-                                                    'The ID of the custom scale or 0'),
-                                                'name' => new external_value(PARAM_RAW, 'The module name'),
-                                                'grademin' => new external_value(PARAM_FLOAT, 'Minimum grade'),
-                                                'grademax' => new external_value(PARAM_FLOAT, 'Maximum grade'),
-                                                'gradepass' => new external_value(PARAM_FLOAT,
-                                                    'The passing grade threshold'),
-                                                'locked' => new external_value(PARAM_BOOL,
-                                                    'false means not locked, true is a lock'),
-                                                'hidden' => new external_value(PARAM_BOOL,
-                                                    'false means not hidden, true hide until'),
-
-                                            )
-                                        ),
                                         'str_grade' => new external_value(PARAM_RAW, 'String declaration of grade'),
                                         'str_long_grade' => new external_value(PARAM_RAW,
                                             'String declaration of grade'),
